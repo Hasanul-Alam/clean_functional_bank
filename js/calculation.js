94ad52a782cad83196fdb1172328a0fcc08ca68a
Hasanul Alam Prince <74726194+Hasanul-Alam@users.noreply.github.com>
2021-08-24 00:39:08 +0600

    janotano coding

--- js/calculation.js
+++ js/calculation.js
@@ -5,11 +5,29 @@ function getInputValue(inputId) {
     input.value = '';
     return inputValue;
 }
-function getCurrentValue(inputId) {
-    const currentValue = document.getElementById(inputId);
+function getCurrentValue(textId) {
+    const currentValue = document.getElementById(textId);
     const currentValueText = currentValue.innerText;
     const currentValueNumber = parseFloat(currentValueText);
+    return currentValueNumber;
+}
+function updateTotalField(textId, inputId) {
+    const previousAmount = getCurrentValue(textId);
+    const newAmount = getInputValue(inputId);
+    const totalAmount = previousAmount + newAmount;
+    document.getElementById(textId).innerText = totalAmount;
+    return totalAmount;
+}
+function updateBalanceField() {
+    const previousAmount = getCurrentValue('balance_total');
+    const previousAmountText = previousAmount.innerText;
+    const previousAmountNumber = parseFloat(previousAmountText);
+
 }
 document.getElementById('deposit_btn').addEventListener('click', function () {
-    getInputValue('deposit_input');
+    updateTotalField('deposit_total', 'deposit_input');
+    updateTotalField('balance_total', 'deposit_input');
+});
+document.getElementById('withdraw_btn').addEventListener('click', function () {
+
 });
